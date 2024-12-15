da179702af432edfefdd1ff745ae5aae436ebf91
zodihax <gerund-frugal-0m@icloud.com>
2024-12-15 01:51:28 +0100

    data parser

--- backend/core/plex/data_parser.py
+++ backend/core/plex/data_parser.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from pydantic import BaseModel, Field
 
-from core.base.database.models.media import MediaCreate
+from core.base.database.models.media import MediaUpdate
 
 
 class PlexDataParser(BaseModel):
@@ -11,10 +11,10 @@ class PlexDataParser(BaseModel):
     connection_id: int = Field(default=0)
     title: str = Field()
     year: int = Field()
-    rating_key: int = Field(validation_alias="ratingKey")
-
-
-def parse_media(connection_id: int, media_data: dict[str, Any]) -> MediaCreate:
+    plex_rating_key: int = Field(validation_alias="ratingKey")
+    plex_trailer_exists: bool | None = Field(default=False)
+    
+def parse_media(connection_id: int, media_data: dict[str, Any]) -> MediaUpdate:
     """Parse the media data from Plex to a MovieCreate object.\n
     Args:
         connection_id (int): The connection id.
@@ -24,5 +24,4 @@ def parse_media(connection_id: int, media_data: dict[str, Any]) -> MediaCreate:
     media_parsed = PlexDataParser(**media_data)
     media_parsed.connection_id = connection_id
 
-    # Skip model validation, since Plex never creates entries
-    return MediaCreate(**media_parsed.model_dump())
+    return MediaUpdate.model_validate(media_parsed.model_dump())
